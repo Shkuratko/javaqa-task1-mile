c0afdabe402b5f65aa48ac20735e3898a14e746e
Yuliya Shkuratko <sm0ci@mail.ru>
2021-10-28 22:35:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 1200;
+        int rublesForBonusMiles = 20;
+
+        int mile = ticket / rublesForBonusMiles;
+        System.out.println(mile);
+    }
+}
